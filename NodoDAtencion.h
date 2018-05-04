71616e307847c1ed80f2435ae58922ac28fa27b2
Jonathan Vasquez <jonacote12@hotmail.com>
2018-05-04 05:36:10 -0600

    sz

---
+++ NodoDAtencion.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+
+class NodoDAtencion
+{
+    public:
+        NodoDAtencion();
+        NodoDAtencion(long pIdEstudiante, string pCodCurso, string pCodGrupo, int pAccion);
+        virtual ~NodoDAtencion();
+
+    protected:
+
+    private:
+        long idEstudiante;
+        string codGrupo;
+        string codCurso;
+        int accion;
+        NodoDAtencion *previous;
+        NodoDAtencion *next;
+
+    friend class ListaDAtencion;
+    friend class ColaDeAtencion;
+    friend class ListaDMesas;
+    friend class nodoMostrador;
+};
